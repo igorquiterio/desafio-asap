a2aab469de79aeccdc0707a494c344dfe12f2130
Igor Quiterio <idcq101@gmail.com>
2021-01-23 13:52:22 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
+import authProvider from "./providers/authProvider";
+import { UserList } from "./resources/Users";
+import { PostList, PostEdit, PostCreate } from "./resources/Posts";
+import jsonServerProvider from "ra-data-json-server";
+import ptBrMessages from "ra-language-pt-br";
+import polyglotI18nProvider from "ra-i18n-polyglot";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import PostIcon from "@material-ui/icons/Book";
+import UserIcon from "@material-ui/icons/Group";
+
+const messages = {
+  "pt-br": ptBrMessages,
+};
+const i18nProvider = polyglotI18nProvider(
+  (locale) => messages[locale],
+  "pt-br"
+);
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+
+const App = () => (
+  <Admin
+    authProvider={authProvider}
+    dataProvider={dataProvider}
+    i18nProvider={i18nProvider}
+  >
+    <Resource
+      name="posts"
+      list={PostList}
+      edit={PostEdit}
+      create={PostCreate}
+      icon={PostIcon}
+    />
+    <Resource name="users" list={UserList} icon={UserIcon} />
+  </Admin>
+);
 
 export default App;
